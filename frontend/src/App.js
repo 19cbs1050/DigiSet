4c953dcfbdd8649d5c9267d37aecc0f6e8afd378
sahil2311sharma <sahilsharma123027@gmail.com>
2022-03-05 03:44:42 +0530

    Assets Page Ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,30 @@
-import React from "react";
-import { Container} from "react-bootstrap";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-import './index.css'
-const App = () =>{
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const App = () => {
   return (
-    <>
+    <div>
+      <Router>
         <Header/>
-        <main 
-          className="py-3 d-flex align-items-center justify-content-center" 
-          style={{backgroundColor:"black",display: 'flex', justifyContent: 'center'}}
-        >
-          <Container>
-            <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomeScreen />} exact/>
-                <Route path="/product/:id" element={<ProductScreen />}/>
-            </Routes> 
-          </BrowserRouter>
-            
-          </Container>
-          
-        </main>
-        <Footer/>
-    </>
+      <main 
+      //className='py-3'
+      className="py-3 d-flex align-items-center justify-content-center" 
+      style={{backgroundColor:"black",display: 'flex', justifyContent: 'center'}}
+      >
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
+    </div>
   )
 }
 
-export default App;
+export default App
--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,18 +1,33 @@
 import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
 const Header = () => {
   return (
-    <div>
-      <Navbar bg="dark" variant="dark">
-      <Container>
-      <Navbar.Brand href="/">DigiSet</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="/Cart"><i className='fas fa-shopping-cart'/>Cart</Nav.Link>
-        <Nav.Link href="/SignIn"><i className='fas fa-user'/>SignIn</Nav.Link>
-      </Nav>
-      </Container>
-    </Navbar>
-    </div>
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   )
 }
 
