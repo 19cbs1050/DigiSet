c9b040e8f255ac8c29ba9a83c5bab00217d50514
sahil2311sharma <sahilsharma123027@gmail.com>
2022-03-05 19:36:34 +0530

    Fixed routes bug of components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
