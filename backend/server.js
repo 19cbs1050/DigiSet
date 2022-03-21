17b951b07e7456615317ca3036012388c3667703
sahil2311sharma <sahilsharma123027@gmail.com>
2022-03-21 14:37:34 +0530

    Database Configuration + tokens part completed

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,39 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
 connectDB()
-
 const app = express()
-
-
-app.get('/',(req,res)=>{
-    res.send('API is running')
-})
-app.get('/api/products',(req,res)=>{
-    res.json(products)
+app.get('/', (req, res) => {
+  res.send('API is running....')
 })
 
+app.use('/api/products', productRoutes)
 
+app.use((req,res,next) =>{
+    const error = new Error(`Not found - ${req.originalUrl}`)
+    res.status(404)
+})
 
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
+app.use((err,req,res,next) =>{
+    const statusCode = res.statusCode ===200? 500: res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production'? null: err.stack,
+    })
 })
 
 const PORT = process.env.PORT || 5000
 
 app.listen(
-    PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`)
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
