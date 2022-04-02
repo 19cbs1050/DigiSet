1d1ec080c51def1bcd03f2d8fc28eff119d4e843
sahil2311sharma <sahilsharma123027@gmail.com>
2022-04-02 22:05:50 +0530

    handled middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -8,12 +9,18 @@ import productRoutes from './routes/productRoutes.js'
 dotenv.config()
 
 connectDB()
+
 const app = express()
+
 app.get('/', (req, res) => {
   res.send('API is running....')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.use((req,res,next) =>{
     const error = new Error(`Not found - ${req.originalUrl}`)
