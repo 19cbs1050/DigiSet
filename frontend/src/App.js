f5e7e8ea8912fcec959f8c770434445f43d74ea9
sahil2311sharma <sahilsharma123027@gmail.com>
2022-04-17 23:26:50 +0530

    Order Summary Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Header/>
-      <main 
-      className="py-3 d-flex align-items-center justify-content-center" 
-      style={{backgroundColor:"black",display: 'flex', justifyContent: 'center'}}
-      >
+    <Router>
+      <Header />
+      <main className='py-3'>
         <Container>
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
@@ -28,10 +30,8 @@ const App = () => {
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </Router>
-    </div>
   )
 }
-
 export default App
