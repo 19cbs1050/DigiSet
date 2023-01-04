cdebe82d2360b7a70d4b95e0e34574d6ef396fb1
sahil2311sharma <sahilsharma123027@gmail.com>
2023-01-05 01:45:35 +0530

    change bg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
       <Header />
       <main 
       className="py-3 d-flex align-items-center justify-content-center" 
-      style={{backgroundColor:"#f9bcbc",display: 'flex', justifyContent: 'center'}}
+      style={{backgroundColor:"#000",display: 'flex', justifyContent: 'center'}}
       >
         <Container>
           <Route path='/order/:id' component={OrderScreen} />
