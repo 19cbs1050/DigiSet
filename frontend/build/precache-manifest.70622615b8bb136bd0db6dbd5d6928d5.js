c800db30c308b007189e192f32619b091e6cdf4e
sahil2311sharma <sahilsharma123027@gmail.com>
2023-01-08 00:44:09 +0530

    test

---
+++ frontend/build/precache-manifest.70622615b8bb136bd0db6dbd5d6928d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046e9bea30f43e984874250a31bdf575",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b80b31f57ee5da90c97e",
+    "url": "/static/css/main.5830a620.chunk.css"
+  },
+  {
+    "revision": "e15b5eebdff4dcf103f9",
+    "url": "/static/js/2.cde6176b.chunk.js"
+  },
+  {
+    "revision": "ab955ab48e1139d7f6811093d70f1817",
+    "url": "/static/js/2.cde6176b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80b31f57ee5da90c97e",
+    "url": "/static/js/main.d29b5d4b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.d29b5d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
